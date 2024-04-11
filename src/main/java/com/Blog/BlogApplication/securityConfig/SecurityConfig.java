09fb30b01f483754dfc5f43ec5fba08a603d8fa1
Arun5428 <arunkumarkpm69@gmail.com>
2024-04-11 10:08:38 +0530

    dokerfile altered

--- src/main/java/com/Blog/BlogApplication/securityConfig/SecurityConfig.java
+++ src/main/java/com/Blog/BlogApplication/securityConfig/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.Blog.BlogApplication.securityConfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @Configuration
+
 public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
